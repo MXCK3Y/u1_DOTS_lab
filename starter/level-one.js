7fc9c004c41526bd1627d212cfb50000d92fdcac
MXCK3Y <mickeymurlyn@gmail.com>
2024-09-23 19:54:28 -0500

    completed Dots lab

--- starter/level-one.js
+++ starter/level-one.js
@@ -1,4 +1,43 @@
-/*
- * DOTS: Level One
- *
- */
+// LVL 1
+
+let score = 0
+const ball = document.querySelector(`.js-ball`)
+const win= document.querySelector(`.level-winner`)
+let scoreDisplay = document.querySelector(`.js-score`)
+
+ball.addEventListener(`click`, () =>{
+    scoreDisplay.innerText = score += 10
+    if (score > 99) {win.style.opacity = 1 }
+})
+
+
+
+
+//Disclaimer
+
+// i went through so many versions of codes for all 3 levels. in hindsight i wish i 
+// would have kept track of all of them. i pretty much started the 1st lvl 
+// with help from my labmates. When everyone shared their code in the main zoom room i took bits and pieces 
+// from them. Mike's use of the forEach loop made me go watch videos to get a better 
+// understanding of them. i incorporated that into my 2nd and 3rd level. My original 
+// 3rd level was pretty much verbatim Molly's. But i didnt feel satisfied with just copying so i 
+// tried to decipher it. Upon doing so i took what i learned from my 2nd level and redid 
+// it. After that i jumped in office hours with tom and used chat gpt to help me figure 
+// out how to shorten it as much as possible. 
+
+// Ex1  instead of this:
+// score =score + 10
+// scoreDisplay.innerText = score
+
+// i learned to use += to get
+// scoreDisplay.innerText = score += 10
+
+
+//EX2 
+// if the game ends at 100 points at every stage it made sense to keep
+// if (score > 99) {win.style.opacity = 1 }
+// and redo every version that had an else condtion 
+
+//EX3
+// i spent time commenting out certain parts to see how it reacted which helped me identify
+// a lot of fluff
--- starter/level-three.js
+++ starter/level-three.js
@@ -1,4 +1,34 @@
-/*
- * DOTS: Level Three
- *
- */
+
+
+score = 0
+let allBalls = document.querySelectorAll(`.js-ball`)
+let win = document.querySelector (`.level-winner`)
+let scoreDisplay = document.querySelector(`.js-score`)
+
+allBalls.forEach (ball => {
+    ball.addEventListener(`click`, () =>{
+        scoreDisplay.innerText = score += Number(ball.dataset.increment)
+        if (score > 99) {win.style.opacity = 1 }
+    })})
+
+
+
+
+    // My old solution with for loop
+
+// I realized that when adding my score i could get rid of the [i] index attached to allBalls[i]
+// if i used my var ball with the .dataset.increment attached to it. That allowed me to delete
+// the for loop entirely and essentially use the same code from lvl 2 
+
+
+    // score = 0
+// let allBalls = document.querySelectorAll(`.js-ball`)
+// let win = document.querySelector (`.level-winner`)
+// let scoreDisplay = document.querySelector(`.js-score`)
+
+// for (let i =0; i < allBalls.length; i++) {
+//     allBalls[i].addEventListener(`click`, function() {
+//             scoreDisplay.innerText =score += Number(allBalls[i].dataset.increment)
+//             if (score > 99) {win.style.opacity = 1 }
+//     })
+// }
