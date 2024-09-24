0c5f77f298993635959bc8bbae843944b54d54fa
MXCK3Y <mickeymurlyn@gmail.com>
2024-09-24 08:04:50 -0500

    done

--- starter/level-two.js
+++ starter/level-two.js
@@ -1,8 +1,6 @@
 
     // I started with a different solution to lvl 2.  I decided to copy the code from lvl 1 
-    // and try to add as little as possible to build some continuity. Mike's solution used .forEach
-    // that inspired me to look up videos to get a better understanding on forEach loops. Which led
-    // to this version of code 
+    // and try to add as little as possible to build some continuity.
 
     let score = 0
     const balls = document.querySelectorAll(`.js-ball`)
